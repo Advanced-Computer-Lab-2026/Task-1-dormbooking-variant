0a78ddc481291fc63db94c28ec723e941c39ef7a
Gebaly22 <abdelrhman.22elgebaly@gmail.com>
2026-09-22 10:52:33 +0300

    Task 1 done

--- server/src/routes/bookings.js
+++ server/src/routes/bookings.js
@@ -3,12 +3,16 @@ import {
   getAllBookings,
   getBooking,
   createBooking,
-  updateBooking,
+  updateBooking,  
   deleteBooking
 } from '../controllers/bookingController.js';
 
 const router = Router();
 
-// TODO: wire up the routes described in README.md section 3.
+router.get('/', getAllBookings);
+router.get('/:id', getBooking);
+router.post('/', createBooking);
+router.patch('/:id', updateBooking);
+router.delete('/:id', deleteBooking);
 
 export default router;
